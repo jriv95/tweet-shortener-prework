2c0d89e31cb1388ba6104fd3177a3312f7f5de0f
Joel Rivera <joel.rivera7995@gmail.com>
2018-10-16 15:34:02 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -42,7 +42,7 @@ end
 def shortened_tweet_truncator(tweet)
   tweet.split(" ").collect do |post|
     if post.length > 140
-      word_substituter(post)[0...140] + ".."
+      word_substituter(post)[0...140] + "..."
     else
       post
     end
