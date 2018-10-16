09dac3a88474ac408796b34629ffe6d29d939181
Joel Rivera <joel.rivera7995@gmail.com>
2018-10-16 16:16:36 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -17,7 +17,7 @@ def word_substituter(tweet)
   tweet.split(" ").collect do |word|
       if dictionary.keys.include?(word.downcase)
         word = dictionary[word.downcase]
-        bing.pry
+        binding.pry
       else
         word
     end
